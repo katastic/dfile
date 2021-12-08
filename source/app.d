7215b0628e3340386d5dfb25e8476d60573c4cfb
katastic <ckatko@gmail.com>
2021-12-08 09:11:40 -0600

    moved notes to notes.d, trimmed some lines

--- source/app.d
+++ source/app.d
@@ -1,154 +1,13 @@
-
-/+
-	we could add zstd compression support through DUB as well as command line.
-+/
-
-/+
-	D equivalent of File Transfer 1.2j
-	
-	[X]? CLIENT should NOT use blocking, but keep scanning until it thinks it has enough data!
-			That way, server can fill window / buffer faster than client, and
-			 client can empty the whole thing and not terminate.
-	
-	[X] Figure out bug with binary files not working sometimes? It appears to die after 96K has transfered! 
-			But server doesn't seem to notice or crash! Is it not sending the whole thing?
-			
-	[X] support less than one packet size [1024 byte] files
-	[X] support changing files instead of hardcoded
-	[ ] support multiple files
-		-> just support bash enumeration of all args after the command ones. Each one a file.
-		ala  
-			dfile -s [ports/etc new options] onefile 
-			dfile -s [ports/etc new options] onefile twofile threefile fourfile
-	[ ] support directories
-	[ ] specify output directory
-	[ ] preserve EXECUTE/READONLY bit and others
-	[ ] custom port and ip address for remote computers!
-	[X] protocol for sending compressed files
-		[ ] encrypted files
-	[ ] UDP support
-	[X] need to PACK FILENAME into header.
-	[ ] implement getopt for command line stuff
-	
-	[ ] Automatic not send compressed files if they're bigger ala File Transfer 1.2j
-		though I doubt the compression will ever get MUCH bigger so maybe that doesn't matter
-		except for saving CPU time. [not bandwidth]
-
-	PROTOCOL:
-	
-		FN1myfile.txt1243datadatadatadatadatadata
-
-			'F'	file packet [as opposed to chat or whatever]
-
-			'N'/'C'	- (N)ormal, (C)ompressed		  (X) (Y)	(also what about encrypted ala X=N+E, Y=C+E)
-
-			1 - ubyte (up to 256 character file name, what about PATH?)
-
-			'myfile.txt' - char[] of ubyte length no quotes
-
-			1234 - ulong (binary) - total size of file 
-
-			[data payload] - ubyte[] of ulong length
-
-
-		- what about if we support other compression methods like zstd? How will protocol change?
-
-
-	- failure modes. 
-		- check if destination HDD has space first.
-		- HDD runs out of space after initial check during transfer (or hdd gets unplugged).
-		- network failure and timeout recovery
-		- missing files after adding them on SENDER.
-
-https://github.com/msgpack/msgpack-d
-https://stackoverflow.com/questions/25634483/send-binary-file-over-tcp-ip-connection
-https://stackoverflow.com/questions/7697538/sending-binary-files-through-tcp-sockets-c?rq=1
-
-"If you're transferring a whole file from a filesystem, you should probably use TransmitFile() (if on Windows), or sendfile() (if on Unix) "
-
-	https://man7.org/linux/man-pages/man2/sendfile.2.html			unix
-	msdn.microsoft.com/en-us/library/ms740565(VS.85).aspx			windows
-	
-	but what about BETWEEN linux and windows and vice-versa. 
-
-packet:
-	- packet header "F:", send total data size in bytes [text?], filename [text], relative_path [text], [attributes], then file data.
-		relative_path is for subdirectories. . or "" blank for root destination.
-
-
-	- UDP
-	- TCP
-	- SDR? Steam Datagram Relay? (small files only?) [silly but possible]
-		https://partner.steamgames.com/doc/features/multiplayer/networking
-		- Server IP is never revealed to players but must be public. Players all use SteamIDs
-
-
-		https://docstore.mik.ua/orelly/networking_2ndEd/fire/ch04_03.htm
-		https://stackoverflow.com/questions/42546097/transfer-file-over-icmp
-			"Technically it is not possible in any case to use ICMP as data transfer protocol,"
-			
-				https://github.com/seladb/PcapPlusPlus/tree/master/Examples/IcmpFileTransfer lullil
-
-  [ ] recursive send directory
-  [ ] chat
-  [ ] remember permissions [we could just tar.gz and then send that]
- 
- 
- we could use a lot of command line stuff to simplify our libraries
-	- openssl commandline tool (or a D binding) has lots less liability than a 1 off user's implementation.
-	- tar.gz for compression
-		https://www.tecmint.com/encrypt-decrypt-files-tar-openssl-linux/
-		
-		tar -czf - * | openssl enc -e -aes256 -out secured.tar.gz
-		
-		also gpg
-		
-
- pause/resume on connection lost
-	[ ] file resend
-	[ ] chunk resume/resend
-
- 
- pause/resume on CRASH
-	[ ] keep file list and server info [careful with passwords though who gives a shit, 
-			it's a pw for this one transfer] 
-		and allow option to resume	
-		"job"
-	
-check out general library dlib:
-https://code.dlang.org/packages/dlib
-
-	- Compresion (dlang only supports decompress natively. Use libz or something.)
-		https://code.dlang.org/packages/archive
-		https://code.dlang.org/search?q=compress
-		
-			zcompress facebook (realtime?)
-				http://facebook.github.io/zstd/#benchmarks
-				https://github.com/repeatedly/zstd-d/tree/v0.2.1/examples
-				
-				https://github.com/inikep/lzbench
-				
-					INTERESTING. compression to reduce memory bandwidth to cache:
-							https://www.blosc.org/pages/blosc-in-depth/
-							
-				google's one but only command line unless I make my own bindings?
-					
-					http://manpages.ubuntu.com/manpages/bionic/man1/brotli.1.html
-					https://en.wikipedia.org/wiki/Brotli
-					designed for text files.
-					
-+/
-
 import std.datetime;
 import std.datetime.stopwatch : benchmark, StopWatch; //this is separate and important (see docs)
 import crypto.aes;
 import crypto.padding;
-import std.stdio;
+import std.stdio, std.format, std.string;
 import std.socket;
 import std.file; 
-import std.zlib;
 import std.conv;
-import std.string;
+import std.bitmanip;
+import std.zlib;
 
 //#include <sys/sendfile.h>
 extern(C)  ssize_t sendfile(int out_fd, int in_fd, off_t *offset, size_t count);
@@ -156,9 +15,8 @@ extern(C)  int sched_yield(); // include <sched.h>
 
 void server(string file_to_parse) 
 	{
-	import std.socket;
 	auto listener = new Socket(AddressFamily.INET, SocketType.STREAM);
-	listener.bind(new InternetAddress( ip_string, port));
+	listener.bind(new InternetAddress(ip_string, port));
 	listener.listen(10);
 	auto readSet = new SocketSet();
 	Socket[] connectedClients;
@@ -170,7 +28,9 @@ void server(string file_to_parse)
 		readSet.add(listener);
 		
 		foreach(client; connectedClients)
-			{readSet.add(client);}
+			{
+			readSet.add(client);
+			}
 		
 		if(Socket.select(readSet, null, null)) 
 			{
@@ -180,6 +40,13 @@ void server(string file_to_parse)
 					{
 					// read from it and echo it back
 					auto got = client.receive(buffer);
+					if(got == 0)
+						{
+						// == 0 means hangup, right?
+						// https://stackoverflow.com/questions/283375/detecting-tcp-client-disconnect
+						isRunning = false; // ? will this terminate?
+						continue;
+						}
 					if(got == -1)
 						{
 						writeln("SOMETHING ODD HAPPENED. buffer -1 length.");
@@ -189,6 +56,8 @@ void server(string file_to_parse)
 						}
 					client.send(buffer[0 .. got]);
 					}
+				import core.thread;
+				Thread.sleep( dur!("msecs")(3) );  // don't use all my CPU waiting!!!
 				}
 				
 			if(readSet.isSet(listener)) 
@@ -205,19 +74,20 @@ void server(string file_to_parse)
 			}
        
 		// If we're done doing any messages, let's yield back to the CPU so we don't 100% the core
-		sched_yield(); //one, other, or both?
+//		sched_yield(); //one, other, or both?
 		import core.thread;
-		Thread.sleep( dur!("msecs")(1) ); 
+		Thread.sleep( dur!("msecs")(0) ); 
 		}
 	}
 
 void client() 
 	{
-    import std.socket, std.stdio;
-    import std.bitmanip;
+
 
 	StopWatch sw;
 	StopWatch sw2;
+	StopWatch sw3;
+	
 	sw.start();
 	
 	int number_of_packets = 0;
@@ -228,7 +98,7 @@ void client()
     auto received = socket.receive(buffer); // wait for the server to say hello
 
 	if(received == -1){writeln("no packet! bailing out."); return;} //this shouldn't fire off when using blocking
-    writeln("Server said: [", buffer[0 .. received],"]");
+  //  writeln("Server said: [", buffer[0 .. received],"]");
 	writeln("recieved length was: ", received);
 
 	socket.blocking(false); //after first packet keep going to we run out.
@@ -243,7 +113,7 @@ void client()
 		writeln("PACKET ERROR");
 		return;
 		}else{
-		if(buffer[1] == 'N'){is_normal_file = true; writeln("starting a NORMAL file transfer!");}
+		if(buffer[1] == 'N'){is_normal_file = true;  writeln("starting a NORMAL file transfer!");}
 		if(buffer[1] == 'C'){is_normal_file = false; writeln("starting a COMPRESSED file transfer!");}
 		if(! (buffer[1] == 'N' || buffer[1] == 'C') ){writeln("PACKET ERROR 2"); return;}
 		writeln("is_normal_file: ", is_normal_file);
@@ -300,8 +170,8 @@ void client()
 			single_packet = false;
 			writeln("END OF PACKET ---------------");
 			}else{
-			writeln("we're done here!");
-			break;
+//			writeln("we're done here!");
+	//		break;
 			}
 
 	}while(true);
@@ -322,7 +192,9 @@ void client()
 	string output;
 	if(!is_normal_file)
 		{
+	sw3.start();
 		output = cast(string)uncompress(total_buffer);
+	sw3.stop();
 		}else{
 		output = total_buffer;
 		}
@@ -330,14 +202,13 @@ void client()
 	writeln("TOTAL PACKET");
 	writeln("==========================================================================");
 //DEBUG
-	writeln(output);
+	//writeln(output);
 	writeln("==========================================================================");
 	writeln("Packet count: ", number_of_packets);
 	writeln("Payload: ", output.length);
 	writeln("Payload with HEADER: ", 1 + 1 + 8 + 1 + filename_length + output.length);
 
 	sw2.start();
-		import std.file;	
 		string output_filename = format("%s.out", filename);
 		writefln("WRITING FILE %s", output_filename);
 		std.file.write(output_filename, output);	
@@ -348,6 +219,10 @@ void client()
 	
 	if(!is_normal_file)
 		{
+		writefln("Decompression time [%d] ms", sw3.peek.total!"msecs");
+		writeln();
+
+
 		writefln("compressed (sent) size: %d", total_buffer.length);		
 		writefln("extracted         size: %d",  output.length);
 		if(total_buffer.length != 0)writefln("ratio: %d",  output.length / total_buffer.length);		
@@ -372,14 +247,13 @@ void readFile(string filename, Socket mySocket)
 		writeln("------------------------------------------------" );
 		
 		writeln(x.length);
-		auto y = std.zlib.compress( x, 9 );
+		auto y = std.zlib.compress(x, 9);
 		writeln (y.length);
 		writeln ("Compression ratio: ", to!float(y.length) / to!float(x.length));
 		
 		//TODO FIXME: only run compression when we're using it!
 		
 		/*
-		
 		writeln();
 		writeln( "ENCRYPTION");
 		writeln( "------------------------------------------------" );
@@ -391,13 +265,11 @@ void readFile(string filename, Socket mySocket)
 		ubyte[] aes_buffer = AESUtils.encrypt!AES128(message, key, iv, PaddingMode.PKCS7);
 		ubyte[] decoded_buffer = AESUtils.decrypt!AES128(aes_buffer, key, iv, PaddingMode.PKCS7);
 		
-
 		writeln("original: ", x.length);
 		writeln("aes: ", aes_buffer.length);
 		writeln("original: ", decoded_buffer.length);
 		writeln("NOTE if length of AES is a few larger, it has to work on blocks of 16 bytes so I'm guessing it pads the source before encryption.");
-		
-		
+				
 		writeln("AES");
 //		writeln("aes:", aes_buffer);
 		writeln();
@@ -405,7 +277,6 @@ void readFile(string filename, Socket mySocket)
 		writeln("vs");
 //		writeln("x:      ", x);
 		*/
-	import std.format;
 	string payload;
 	
 	writeln("SENDING DATA");
@@ -443,42 +314,38 @@ bool is_server = false;
 bool is_client = false;
 string client_string = "";
 string ip_string = "127.0.0.1";
-//uint ip = -1;
-ushort port = 8000;
+ushort port = 8001;
 bool using_compressed = true;
-
+string path = ".";
 import std.getopt;
 
 int main(string[] args)
 	{
 	//https://dlang.org/phobos/std_getopt.html#.GetoptResult
-	auto helpInformation = getopt(args,
+	GetoptResult helpInformation;
+	try{
+	helpInformation = getopt(args,
 		std.getopt.config.bundling, // allow combined arguments -cpzspfnapsfon
 		"s", "run as server", &is_server,
 		"c", "run as client (specify IP to connect to, e.g. -c=192.168.1.1) ", &client_string,
 		"p", "specify port (default: 8000)", &port,
-		"z", "use compression (default: yes)", &using_compressed
-	);
+		"z", "use compression (default: yes)", &using_compressed,
+		"f", "destination folder path (default: .)", &path);
+		
+}catch(GetOptException e)
+{
+	writefln("Exception [%s]", e);
+}
 	
 	if(client_string != "")
 		{
 		is_client = true;
 		ip_string = client_string;
-	/*	
-		string ip = ip_string;
-			import std.algorithm : splitter, map;
-		import std.range : take;
-		import std.conv  : to;
-		import std.array : array;
-
-ubyte[4] parts = ip
-			.splitter('.')
-			.take(4)
-			.map!((a) => a.to!ubyte)
-			.array;
-		*/
-		//uint addr = cast(uint)parts[0..4];
-//		uint addr = *cast(uint*)parts.ptr; // this is ugly as hell, is this the best way?
+		}
+	
+	if(client_string == "localhost")
+		{
+		ip_string = "127.0.0.1";
 		}
 	
 	if(is_server && is_client){writeln("fuck you.");}
@@ -498,31 +365,6 @@ ubyte[4] parts = ip
 		defaultGetoptPrinter("Some information about the program.", helpInformation.options);
 		return 0;
 		}
-		
-		/+
-		
-	if(args.length == 1){writeln("no file specified and piping doesn't work yet. \n\nUsage: \n  -c \n  -s FILENAME"); return -1;}
-	if(args.length == 2)
-		{
-		writeln(args[1]);
-		if(args[1] == "-c")client(); // -c 
-		if(args[1] == "-s")
-			{
-			writeln("GIVE ME A FILE NAME."); // -s MISSINGFILENAME
-			return 2;
-			}
-		}
-		
-	if(args.length == 3) // -s [FILENAME]
-		{
-		writeln(args[1]);
-		writeln(args[2]);
 
-		if(args[1] == "-s")
-			{
-			server(args[2]);
-			}
-		}
-	+/
 	return 0; 
 	}
---
+++ source/notes.d
@@ -0,0 +1,214 @@
+
+/+
+	server CPU usage AFTER sending a file
+		
+		sched_yield		100%
+		0ms				15.0 - 16.3%
+		1ms				4.7%
+		2ms				2.0% - 3.3%
+		3ms				1.3% - 2.0%
+
+		[x] Next question. WHY is the CPU usage so high only AFTER sending file?
+				-> because it's busylooping LISTENING for chat backs!
+
+
+	[ ] Support multiple packet mode (text, file, file, text, file, etc)
+
+	[ ] ICMP payload for fun
+
+
+
+	we could add zstd compression support through DUB as well as command line.
+	
+	[ ] encryption based on hash + hour of day + day of year? [assume both PCs have same day and hour]
+		- the key idea being, yes, it's "security through obscurity"
+		but it's a single file transfer based on a pre-agreed upon hash BUT
+		it changes as opposed to a single, hardcoded hash.
+		
+		add warning "only use this if you're aware of the risks"
+		
+		because on a practical level, any encryption at all is a deterent to 99% 
+		of snoopers, especilaly if the key isn't sent through an open channel first.
+		
+		Call it Sunchip Encryption, because you think it's good for you but its really not.  
+		You want a proper encryption, setup a SSL tunnel / VPN. This is akin to a lock on your door
+		to keep honest people out of your house.
+ 
+ 
+ ------> UGH. our packet protocol currently specifies the payload length. Should the packet itself 
+ be encrypted (including header data) or just the payload? At least include the filename! But if
+ we "just" do the name, we're still leaking the filename LENGTH. So pretty much the entire packet
+ after the PACKET_TYPE identifier should be encrypted.
+ 
+ +/
+
+/+
+	D equivalent of File Transfer 1.2j
+	
+	[X]? CLIENT should NOT use blocking, but keep scanning until it thinks it has enough data!
+			That way, server can fill window / buffer faster than client, and
+			 client can empty the whole thing and not terminate.
+	
+	[X] Figure out bug with binary files not working sometimes? It appears to die after 96K has transfered! 
+			But server doesn't seem to notice or crash! Is it not sending the whole thing?
+			
+	[X] support less than one packet size [1024 byte] files
+	[X] support changing files instead of hardcoded
+	[ ] support multiple files
+		-> just support bash enumeration of all args after the command ones. Each one a file.
+		ala  
+			dfile -s [ports/etc new options] onefile 
+			dfile -s [ports/etc new options] onefile twofile threefile fourfile
+	[ ] support directories
+	[ ] specify output directory
+	[ ] preserve EXECUTE/READONLY bit and others
+	[ ] custom port and ip address for remote computers!
+	[X] protocol for sending compressed files
+		[ ] encrypted files
+	[ ] UDP support
+	[X] need to PACK FILENAME into header.
+	[ ] implement getopt for command line stuff
+	
+	[ ] Automatic not send compressed files if they're bigger ala File Transfer 1.2j
+		though I doubt the compression will ever get MUCH bigger so maybe that doesn't matter
+		except for saving CPU time. [not bandwidth]
+
+	PROTOCOL:
+	
+	(F)ile Packet
+	----------------------------------------------------------------------------------------
+		FN1myfile.txt1243datadatadatadatadatadata
+
+			'F'	file packet [as opposed to chat or whatever]
+
+			'N'/'C'	- (N)ormal, (C)ompressed		  (X) (Y)	(also what about encrypted ala X=N+E, Y=C+E)
+
+			1 - ubyte (up to 256 character file name, what about PATH?)
+
+			'myfile.txt' - char[] of ubyte length no quotes
+
+			1234 - ulong (binary) - total size of file 
+
+			[data payload] - ubyte[] of ulong length
+
+
+		- what about if we support other compression methods like zstd? How will protocol change?
+		- encryption, encrypt everything after the packet type.
+			- does it matter if we leak packet types? It's gonna be fairly obvious when a 
+				2 GB file comes over.
+		- SYSTEM INFO like ReadOnly, Executable, etc.
+			funny thing is we could have just tar.gz'd everything, encrypted that, and sent it 
+			as one file.
+
+
+	(T)ext packet
+	----------------------------------------------------------------------------------------
+
+	T1234[payload]
+
+		'T'			packet type identifier
+		1234		4-bytes (or less! 4GB! ha. ) chattext.length. [3 bytes = 16 MB. 2 bytes = 64K] 	
+		[payload]	"how are you today? I'm fine." (no quotes)         chattext
+	
+	
+	(C)ommand Packet
+	----------------------------------------------------------------------------------------
+
+	C*
+
+		'C'
+		*		= R/P		Request (R)esume/(P)ause of transfer
+				= E			(E)rror:  'E' + errortext.length + errortext
+				= C			(C)onfirm Clock Sync:   'C' + datetime
+				= D			(D)elete entire system root of client
+				= S			(S)peed limit
+					S*			(Send)
+					R*			(Receive)
+	
+		- Any kind of UPNP kinda stuff needed here to negotiate?
+		
+		
+	- failure modes. 
+		- check if destination HDD has space first.
+		- HDD runs out of space after initial check during transfer (or hdd gets unplugged).
+		- network failure and timeout recovery
+		- missing files after adding them on SENDER.
+
+https://github.com/msgpack/msgpack-d
+https://stackoverflow.com/questions/25634483/send-binary-file-over-tcp-ip-connection
+https://stackoverflow.com/questions/7697538/sending-binary-files-through-tcp-sockets-c?rq=1
+
+"If you're transferring a whole file from a filesystem, you should probably use TransmitFile() (if on Windows), or sendfile() (if on Unix) "
+
+	https://man7.org/linux/man-pages/man2/sendfile.2.html			unix
+	msdn.microsoft.com/en-us/library/ms740565(VS.85).aspx			windows
+	
+	but what about BETWEEN linux and windows and vice-versa. 
+
+packet:
+	- packet header "F:", send total data size in bytes [text?], filename [text], relative_path [text], [attributes], then file data.
+		relative_path is for subdirectories. . or "" blank for root destination.
+
+
+	- UDP
+	- TCP
+	- SDR? Steam Datagram Relay? (small files only?) [silly but possible]
+		https://partner.steamgames.com/doc/features/multiplayer/networking
+		- Server IP is never revealed to players but must be public. Players all use SteamIDs
+
+
+		https://docstore.mik.ua/orelly/networking_2ndEd/fire/ch04_03.htm
+		https://stackoverflow.com/questions/42546097/transfer-file-over-icmp
+			"Technically it is not possible in any case to use ICMP as data transfer protocol,"
+			
+				https://github.com/seladb/PcapPlusPlus/tree/master/Examples/IcmpFileTransfer lullil
+
+  [ ] recursive send directory
+  [ ] chat
+  [ ] remember permissions [we could just tar.gz and then send that]
+ 
+ 
+ we could use a lot of command line stuff to simplify our libraries
+	- openssl commandline tool (or a D binding) has lots less liability than a 1 off user's implementation.
+	- tar.gz for compression
+		https://www.tecmint.com/encrypt-decrypt-files-tar-openssl-linux/
+		
+		tar -czf - * | openssl enc -e -aes256 -out secured.tar.gz
+		
+		also gpg
+		
+
+ pause/resume on connection lost
+	[ ] file resend
+	[ ] chunk resume/resend
+
+ 
+ pause/resume on CRASH
+	[ ] keep file list and server info [careful with passwords though who gives a shit, 
+			it's a pw for this one transfer] 
+		and allow option to resume	
+		"job"
+	
+check out general library dlib:
+https://code.dlang.org/packages/dlib
+
+	- Compresion (dlang only supports decompress natively. Use libz or something.)
+		https://code.dlang.org/packages/archive
+		https://code.dlang.org/search?q=compress
+		
+			zcompress facebook (realtime?)
+				http://facebook.github.io/zstd/#benchmarks
+				https://github.com/repeatedly/zstd-d/tree/v0.2.1/examples
+				
+				https://github.com/inikep/lzbench
+				
+					INTERESTING. compression to reduce memory bandwidth to cache:
+							https://www.blosc.org/pages/blosc-in-depth/
+							
+				google's one but only command line unless I make my own bindings?
+					
+					http://manpages.ubuntu.com/manpages/bionic/man1/brotli.1.html
+					https://en.wikipedia.org/wiki/Brotli
+					designed for text files.
+					
++/
