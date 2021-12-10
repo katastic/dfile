1052be5f170f607f554fc213cd6679fd14323b0f
katastic <ckatko@gmail.com>
2021-12-09 18:59:13 -0600

    trimming

--- source/app.d
+++ source/app.d
@@ -1,18 +1,17 @@
-import std.datetime;
-import std.datetime.stopwatch : benchmark, StopWatch; //this line is separate from std.datetime and important (see docs)
+import std.datetime, std.datetime.stopwatch : benchmark, StopWatch;
 import std.stdio, std.format, std.string;
 import std.file, std.socket;
 import std.conv, std.bitmanip;
 import std.zlib;
 import crypto.aes, crypto.padding;
 
-extern(C) ssize_t sendfile(int out_fd, int in_fd, off_t *offset, size_t count); //#include <sys/sendfile.h>
+//extern(C) ssize_t sendfile(int out_fd, int in_fd, off_t *offset, size_t count); //#include <sys/sendfile.h>
 extern(C) int sched_yield(); // include <sched.h>
 
 void server(string[] file_to_parse, string ip_string, ushort port) 
 	{
 	writeln("MODE: SERVER, with file(s):", file_to_parse);
-		
+	assert(file_to_parse.length > 0);		
 	auto listener = new Socket(AddressFamily.INET, SocketType.STREAM);
 	listener.bind(new InternetAddress(ip_string, port));
 	listener.listen(10);
@@ -66,14 +65,13 @@ void server(string[] file_to_parse, string ip_string, ushort port)
 				writeln("SERVER: CONNECTION STARTED, # files = ", file_to_parse.length);
 					foreach(f; file_to_parse)
 						{
-//	payload = format("FN%r%r%r%r", cast(ubyte)filename.length, x.length, filename, x[0 .. $]);
 						string text = "hello";
 						newSocket.send( format("TC\x00%r%s", text.length, text) ); 
 						readFile(f, newSocket);
 						text = "goodbye";
 						newSocket.send( format("TC\x00%r%s", text.length, text) ); 
 						}
-					// newSocket.send("Hello!\n"); // say hello
+
 					connectedClients ~= newSocket; // add to our list
 					
 					newSocket.send( format("X\x00\x00\x00\x00\x00\x00") ); // sent [close connection] packet.
@@ -81,8 +79,6 @@ void server(string[] file_to_parse, string ip_string, ushort port)
 				}
 			}
        
-		// If we're done doing any messages, let's yield back to the CPU so we don't 100% the core
-//		sched_yield(); //one, other, or both?
 		import core.thread : Thread;
 		Thread.sleep( dur!("msecs")(0) ); 
 		}
@@ -90,185 +86,171 @@ void server(string[] file_to_parse, string ip_string, ushort port)
 
 void client(string ip_string, ushort port) 
 	{
-	StopWatch sw;
-	StopWatch sw2;
-	StopWatch sw3;
-	
-	sw.start();
-//	do{
-		auto socket = new Socket(AddressFamily.INET,  SocketType.STREAM);
-		socket.connect(new InternetAddress(ip_string, port));
+	auto socket = new Socket(AddressFamily.INET,  SocketType.STREAM);
+	socket.connect(new InternetAddress(ip_string, port));	
+	char[SEND_SIZE_MAX] buffer;
+	bool is_normal_file = true;
 		
-		//int number_of_packets = 0;
-		char[SEND_SIZE_MAX] buffer;
-		//ubyte filename_length = 0;
-		//char[] filename;
-		//ulong filesize;
-		bool is_normal_file = true;
-				
-		// [X] THE PROBLEM IS, we may recieve more than one packet worth in our buffer!
-		// We might ALSO recieve... LESS than an entire packet!
-		// fuck this, let's just use a serializer library.
-		
-		bool handle_packet(string data)
+	bool handle_packet(string data)
+		{
+		if(data[0] == 'F')  //file
 			{
-			if(data[0] == 'F')  //file
+			ubyte flen = data[2]; //filename length
+			writeln("flen:", flen);
+			uint plen =  //payload length
+				data[3 ] +
+				data[3 + 1]*256 + 
+				data[3 + 2]*256*256 + 
+				data[3 + 3]*256*256*256;
+			writeln("plen:", plen);
+			
+			string filename = data[3+4+4 .. 3+4+4+flen];
+			string payload = data[3+4+4+flen .. $];
+			
+			if(data[1] != 'C' && data[1] != 'N')
 				{
-				ubyte flen = data[2]; //filename length
-				writeln("flen:", flen);
-				uint plen =  //payload length
-					data[3 ] +
-					data[3 + 1]*256 + 
-					data[3 + 2]*256*256 + 
-					data[3 + 3]*256*256*256;
-				writeln("plen:", plen);
-				
-				string filename = data[3+4+4 .. 3+4+4+flen];
-				string payload = data[3+4+4+flen .. $];
-				
-				if(data[1] != 'C' && data[1] != 'N')
-					{
-					assert(false, "PACKET ERROR in compression identifier!");
-					}
-				
-				if(data[1] == 'C') //compressed file
-					{
-					payload = cast(string)uncompress(payload); 
-					}
+				assert(false, "PACKET ERROR in compression identifier!");
+				}
+			
+			if(data[1] == 'C') //compressed file
+				{
+				payload = cast(string)uncompress(payload); 
+				}
 
-				writefln("filename [[%s]]", filename);
-				if(payload.length < 2000)writefln("payload [[%s]] of len=%d", payload, payload.length);
-				
-				string output_filename = format("%s.out", filename);
-				writefln("WRITING FILE [%s]", output_filename);
-				std.file.write(output_filename, payload);	
-				}
-				
-			if(data[0] == 'T')
-				{
-				}
-				
-				
-			if(data[0] == 'X') return 1; // exit/close connection
+			writefln("filename [[%s]]", filename);
+			if(payload.length < 2000)writefln("payload [[%s]] of len=%d", payload, payload.length);
+			
+			string output_filename = format("%s.out", filename);
+			writefln("WRITING FILE [%s]", output_filename);
+			std.file.write(output_filename, payload);	
+			}
+			
+		if(data[0] == 'T') //text/chat packet
+			{
+			}
+			
+		if(data[0] == 'C') {} //command packet
+			
+		if(data[0] == 'X') return 1; // exit/close connection
+	
+		return 0;
+		}
+	
+	string zbuffer = ""; // everything we have so far
+	bool done = false;		
+	socket.blocking(true);
+	
+	uint current_packet_seek_length = 1253152;
+
+	do{
+		if(zbuffer.length < 2000)writefln("waiting for packet. Buffer (len=%d) is [%s] ", zbuffer.length, zbuffer);
+		auto buffer_length = socket.receive(buffer); // wait for the server to say hello
+		writeln("got : ", buffer_length);
 		
-			return 0;
+		zbuffer ~= buffer[0 .. buffer_length];
+			
+		if(zbuffer.length < 2000)
+			{
+			writefln("buffer: <<%s>> ", buffer[0 .. buffer_length]);
+			writefln("zbuffer: <<%s>> ", zbuffer);
+			}
+//	payload = format("FN%r%r%r%r", cast(ubyte)filename.length, filename, x.length, x[0 .. $]);
+//				      012 
+/+
+						x 1
+						 filename[x]
+						  y 1234
+							data[y]
++/
+
+		chop:
+		if(zbuffer.length < 7) continue; //not enough data to form a packet header!
+
+		if(zbuffer[0] != 'F' && zbuffer[0] != 'X' && zbuffer[0] != 'T')
+			{
+			writefln("malformed packet. We got [%s] for packet header.", zbuffer[0]);
 			}
 		
-		string zbuffer = ""; // everything we have so far
-		bool done = false;		
-		socket.blocking(true);
-		
-		uint current_packet_seek_length = 1253152;
-
-		do{
-			if(zbuffer.length < 2000)writefln("waiting for packet. Buffer (len=%d) is [%s] ", zbuffer.length, zbuffer);
-			auto buffer_length = socket.receive(buffer); // wait for the server to say hello
-			writeln("got : ", buffer_length);
+		if(zbuffer[0] == 'T')
+			{
+			// 01234567890
+			// TC01234texttogo
+			// TC0		-- header (T, the rest doesn't matter. flen = 0)
+			//	  1234	-- payload length=text length
 			
-			zbuffer ~= buffer[0 .. buffer_length];
+			uint payload_length = 
+				zbuffer[3] + 
+				zbuffer[3+1]*256 + 
+				zbuffer[3+2]*256*256 + 
+				zbuffer[3+3]*256*256*256;
 				
-			if(zbuffer.length < 2000)
-				{
-				writefln("buffer: <<%s>> ", buffer[0 .. buffer_length]);
-				writefln("zbuffer: <<%s>> ", zbuffer);
-				}
-	//	payload = format("FN%r%r%r%r", cast(ubyte)filename.length, filename, x.length, x[0 .. $]);
-	//				      012 
-	/+
-						    x 1
-						     filename[x]
-							  y 1234
-							    data[y]
-	+/
+			writefln("TEXT RECEIVED len=%d", payload_length);
+			string payload = zbuffer[3+8 .. 3+8 + payload_length]; 
+			writefln("TEXT RECEIVED [%s] %d", payload, payload.length);
 
-			chop:
-			if(zbuffer.length < 7) continue; //not enough data to form a packet header!
-
-			if(zbuffer[0] != 'F' && zbuffer[0] != 'X' && zbuffer[0] != 'T')
+			if(3+8 +payload.length != zbuffer.length)
 				{
-				writefln("malformed packet. We got [%s] for packet header.", zbuffer[0]);
-				}
-			
-			if(zbuffer[0] == 'T')
-				{
-				// 01234567890
-				// TC01234texttogo
-				// TC0		-- header (T, the rest doesn't matter. flen = 0)
-				//	  1234	-- payload length=text length
-				
-				uint payload_length = 
-					zbuffer[3] + 
-					zbuffer[3+1]*256 + 
-					zbuffer[3+2]*256*256 + 
-					zbuffer[3+3]*256*256*256;
-					
-				writefln("TEXT RECEIVED len=%d", payload_length);
-				string payload = zbuffer[3+8 .. 3+8 + payload_length]; 
-				writefln("TEXT RECEIVED [%s] %d", payload, payload.length);
-
-				if(3+8 +payload.length != zbuffer.length)
-					{
-					writeln(3+8+payload.length, " ",zbuffer.length);
-					writefln("buffer was <<%s>>", zbuffer);
-					zbuffer = zbuffer[3+8 + payload_length .. $];
-					writefln("buffer is now <<%s>>", zbuffer);
-					}else{
-					zbuffer = "";
-					}
-				goto chop;
-				}
-			
-			if(zbuffer[0] == 'X')
-				{
-				socket.close();
-				writeln("we got the kill signal!");
-				return ;
-				}
-
-			ubyte flen = zbuffer[2]; //filename length
-			writeln("flen:", flen);
-
-			if(zbuffer[0] == 'F')
-				{
-				current_packet_seek_length = 
-					3 + 	// 'F' 'N' x (where x=filename length)
-					4 + 	// 1234 bytes of packet length
-					4 + 	// why 4??? is this 8 byte packet length???
-					flen +	// 'filename.txt'
-					zbuffer[3 ] +
-					zbuffer[3 + 1]*256 + 
-					zbuffer[3 + 2]*256*256 + 
-					zbuffer[3 + 3]*256*256*256; 
-				}
-				
-			if(zbuffer[0] == 'T')
-				{
-				current_packet_seek_length = 3 + 4 + 4 + zbuffer[3 ] +
-					zbuffer[3 + 1]*256 + 
-					zbuffer[3 + 2]*256*256 + 
-					zbuffer[3 + 3]*256*256*256; 
-				}
-				
-			writefln("buffer length: <<%d>> ", buffer_length);
-			writefln("current_packet_seek_length: <<%d>> ", current_packet_seek_length);
-			  
-			if(zbuffer.length > current_packet_seek_length)
-				{
-				string pbuffer = zbuffer[0 .. current_packet_seek_length]; //single packet of data;
-				zbuffer = zbuffer[current_packet_seek_length .. $]; // shrink zbuffer
-				writeln("---------------------------------------------------");
-				if(pbuffer.length < 2000)writefln("dealing with packet of [%s]", pbuffer);
-				writeln("---------------------------------------------------");
-				
-				if(handle_packet(pbuffer))done = true;
-				//time to parse!
-				
-				if(zbuffer.length < 2000)writefln("new zbuffer was [%s]", zbuffer);
-				goto chop;
+				writeln(3+8+payload.length, " ",zbuffer.length);
+				writefln("buffer was <<%s>>", zbuffer);
+				zbuffer = zbuffer[3+8 + payload_length .. $];
+				writefln("buffer is now <<%s>>", zbuffer);
 				}else{
-				continue; //we need more data
+				zbuffer = "";
 				}
+			goto chop;
+			}
+		
+		if(zbuffer[0] == 'X')
+			{
+			socket.close();
+			writeln("we got the kill signal!");
+			return ;
+			}
+
+		ubyte flen = zbuffer[2]; //filename length
+		writeln("flen:", flen);
+
+		if(zbuffer[0] == 'F')
+			{
+			current_packet_seek_length = 
+				3 + 	// 'F' 'N' x (where x=filename length)
+				4 + 	// 1234 bytes of packet length
+				4 + 	// why 4??? is this 8 byte packet length???
+				flen +	// 'filename.txt'
+				zbuffer[3 ] +
+				zbuffer[3 + 1]*256 + 
+				zbuffer[3 + 2]*256*256 + 
+				zbuffer[3 + 3]*256*256*256; 
+			}
 			
+		if(zbuffer[0] == 'T')
+			{
+			current_packet_seek_length = 3 + 4 + 4 + zbuffer[3 ] +
+				zbuffer[3 + 1]*256 + 
+				zbuffer[3 + 2]*256*256 + 
+				zbuffer[3 + 3]*256*256*256; 
+			}
+			
+		writefln("buffer length: <<%d>> ", buffer_length);
+		writefln("current_packet_seek_length: <<%d>> ", current_packet_seek_length);
+		  
+		if(zbuffer.length > current_packet_seek_length)
+			{
+			string pbuffer = zbuffer[0 .. current_packet_seek_length]; //single packet of data;
+			zbuffer = zbuffer[current_packet_seek_length .. $]; // shrink zbuffer
+			writeln("---------------------------------------------------");
+			if(pbuffer.length < 2000)writefln("dealing with packet of [%s]", pbuffer);
+			writeln("---------------------------------------------------");
+			
+			if(handle_packet(pbuffer))done = true;
+			//time to parse!
+			
+			if(zbuffer.length < 2000)writefln("new zbuffer was [%s]", zbuffer);
+			goto chop;
+			}else{
+			continue; //we need more data
+			}
+		
 		}while(!done);
 		
 		/+
@@ -300,7 +282,6 @@ void readFile(string filename, Socket mySocket)
 		writeln ("Compression ratio: ", to!float(enc.length) / to!float(norm.length));
 		
 		//TODO FIXME: only run compression when we're using it!
-		
 		
 		writeln();
 		writeln( "ENCRYPTION");
@@ -398,61 +379,37 @@ int main(string[] args)
 	}
 	
 	//because getopts will only add true (to default true), not set true to false.
-	if(disable_encryption)
-		{
-		writeln("Disabling encryption");
-		g.using_encryption = false; 
-		}
-		
-	if(disable_compression)
-		{
-		writeln("Disabling compression");
-		g.using_compression = false; 
-		}
+	if(disable_encryption){ writeln("Disabling encryption"); g.using_encryption = false; }
+	if(disable_compression){ writeln("Disabling compression"); g.using_compression = false; }
 	
-	if(g.client_string != "")
-		{
-		g.is_client = true;
-		g.ip_string = g.client_string;
-		}
-	
-	if(g.client_string == "localhost")
-		{
-		g.ip_string = "127.0.0.1";
-		}
-	
-	if(g.is_server && g.is_client)
-		{
-		writeln("Bad llama."); 
-		defaultGetoptPrinter("Usage\ndfile [opts] file1 [file2 .. etc]", helpInformation.options);
-		return -1;
-		}
-	if(!g.is_server && !g.is_client)
+	if(g.client_string != ""){g.is_client = true; g.ip_string = g.client_string;}
+	if(g.client_string == "localhost"){g.ip_string = "127.0.0.1";}
+
+	if( (!g.is_server && !g.is_client) || (g.is_server && g.is_client))
 		{
 		defaultGetoptPrinter(usageInfo, helpInformation.options);
 		return -1;
 		}
 
-	if(g.is_client)client(g.ip_string, g.port);
+	if(g.is_client)
+		{
+//		if(g.ip_string == "" || g.port == 0) writeln("YOU NEED TO SPECIFY PORT AND CLIeNT");  commented out for quick testing.  
+		client(g.ip_string, g.port);
+		}
+
 	if(g.is_server)
 		{
 		if(args.length > 1)
 			{
 			server(args[1 .. $], g.ip_string, g.port); //getopt removes args that are recognized and leaves the rest
-			}
-		else
-			{
+			}else{
 			writeln("ERROR: You need to specify file(s)!");
 			defaultGetoptPrinter(usageInfo, helpInformation.options);
 			return 0;
 			}
 		}
 		
-	if (helpInformation.helpWanted)
-		{
-		defaultGetoptPrinter(usageInfo, helpInformation.options);
-		return 0;
-		}
+	if (helpInformation.helpWanted){defaultGetoptPrinter(usageInfo, helpInformation.options);return 0;}
 
 	return 0; 
 	}
